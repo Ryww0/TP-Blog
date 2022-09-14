2bbc0fc872f4670eafe3ee749bfd582ffc6f48ca
juju01510 <julien.asinari@protonmail.com>
2022-09-14 16:00:25 +0200

    repo_user_ok

--- App/Repository/IUserRepository.php
+++ App/Repository/IUserRepository.php
@@ -1,1 +1,18 @@
 <?php
+namespace App\Repository;
+
+use App\Model\User;
+
+interface IUserRepository
+{
+    public function add(User $user);
+
+    public function fetchAll() : array;
+
+    public function findById($params): User;
+
+    public function update(User $user);
+
+    public function remove(User $user);
+
+}
--- App/Repository/UserRepository.php
+++ App/Repository/UserRepository.php
@@ -1,1 +1,82 @@
 <?php
+
+
+namespace App\Repository;
+
+use App\Model\User;
+use App\Service\Database;
+
+class UserRepository extends Database implements IUserRepository
+{
+    public function add(User $user): User
+    {
+        $stmt = $this->db->prepare("INSERT INTO user (pseudo, email, password, status) VALUES (:pseudo, :email, :password, :status)");
+        $stmt->bindValue(':pseudo', $user->getPseudo());
+        $stmt->bindValue(':email', $user->getEmail());
+        $stmt->bindValue(':password', password_hash($user->getPassword(), PASSWORD_DEFAULT ));
+        $stmt->bindValue(':status', $user->getStatus());
+        $stmt->execute();
+        $stmt = null;
+    }
+
+    public function fetchAll(): array
+    {
+        $stmt = $this->db->prepare("SELECT * FROM user ORDER BY nom ASC");
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $arr = $stmt->fetchAll();
+        if (!$arr) {
+            throw new PDOException("Could not find user in database");
+        }
+        $stmt = null;
+        $users = [];
+        foreach ($arr as $user) {
+            $u = new User();
+            $u->setId($user['id']);
+            $u->setPseudo($user['pseudo']);
+            $u->setStatus($user['status']);
+            $users[] = $u;
+        }
+        return $users;
+    }
+
+    public function findById($params): User
+    {
+        $stmt = $this->db->prepare("SELECT * FROM user WHERE id = :id");
+        $stmt->bindValue(':id', $params);
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $arr = $stmt->fetch();
+        if (!$arr) {
+            throw new PDOException("Could not find id in database");
+        }
+        $stmt = null;
+        $user = new User();
+        $user->setId($arr['id']);
+        $user->setPseudo($arr['pseudo']);
+        $user->setStatus($arr['status']);
+        return $user;
+    }
+
+    public function update(User $user)
+    {
+        $stmt = $this->db->prepare("UPDATE user 
+                                          SET pseudo = :pseudo 
+                                          email = :email
+                                          password = :password
+                                          WHERE id = :id");
+        $stmt->bindValue(':pseudo', $user->getPseudo());
+        $stmt->bindValue(':email', $user->getEmail());
+        $stmt->bindValue(':password', $user->getPassword());
+        $stmt->execute();
+        $stmt = null;
+    }
+
+    public function remove(User $user)
+    {
+        $stmt = $this->db->prepare("DELETE FROM user WHERE id = :id");
+        $stmt->bindValue(':id', $user->getId());
+        $stmt->execute();
+        $stmt = null;
+    }
+}

d2d7ee85655dd66df8878fd7099a67ac57acfc17
Nitha Thghm <t.nithachan@gmail.com>
2022-09-14 15:28:19 +0200

    file article.php modified

--- Templates/front/pages/article.php
+++ Templates/front/pages/article.php
@@ -1,4 +1,12 @@
-<div class="container-fluid>">
-    <h2></h2>
 
+<div class="container-fluid>">
+    <h2><?= $article->getTitle() ?></h2>
+    <img src='<?= $article->getImage() ?>'>
+    <p><?= $article->getContenu() ?></p>
+</div>
+<h4>Commentaire :</h4>
+<div class="container-fluid>">
+    <div>
+        <p><?= $commentaire->getContenu() ?></p>
+    </div>
 </div>

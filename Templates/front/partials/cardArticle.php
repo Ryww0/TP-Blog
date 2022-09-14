7bee2632152fb03e49335bdef188721ecd41b8d8
Nitha Thghm <t.nithachan@gmail.com>
2022-09-14 14:47:09 +0200

    file cardArticle modified

--- Templates/front/partials/cardArticle.php
+++ Templates/front/partials/cardArticle.php
@@ -1,1 +1,8 @@
-<?php
+<div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="<?= $article->getImage() ?>" alt="Card image cap">
+    <div class="card-body">
+        <h5 class="card-title"><?= $article->getTitle() ?></h5>
+        <p class="card-text" maxlength="150"><?= $article->getContenu() ?></p>
+        <a href="#" class="btn btn-primary">Lire</a>
+    </div>
+</div>

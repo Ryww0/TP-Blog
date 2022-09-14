f224aa70b14320ced9beef6194462861fca3e2b9
Ryww0 <laurent.chaitas@gmail.com>
2022-09-14 16:03:54 +0200

    jjjjj

---
+++ App/Controller/front/ArticleController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\front;
+
+use App\Service\View;
+
+use App\Repository\ArticleRepository;
+
+class HomeController
+{
+    use View;
+
+    private ArticleRepository $articleRepository;
+
+    public function __construct()
+    {
+        $this->articleRepository = new ArticleRepository();
+    }
+
+    public function show($id_article)
+    {
+        $article = $this->articleRepository->findById($id_article);
+        return $this->render(
+            SITE_NAME . ' - ' . $article->getTitle(),
+            'front/pages/article.php',
+            [
+                'article' => $article,
+                'commentaires' => $this->commentaireRepository->fectchAllCommentsByArticleId(),
+                'formComment' => FormCommentaire::builCreateComment()
+            ]);
+    }
+}

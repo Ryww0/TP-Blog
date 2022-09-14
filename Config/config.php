4e1d33702d9358d483fee0c51f80ab4f3408bb70
Ryww0 <laurent.chaitas@gmail.com>
2022-09-14 13:56:26 +0200

    initialized

---
+++ Config/config.php
@@ -0,0 +1,15 @@
+<?php
+
+//nom du site
+const SITE_NAME = 'Rencontres sportives';
+
+//App Root
+define("APP_ROOT", dirname(__FILE__, 2));
+const URL_ROOT = '/TP-citation/';
+const URL_SUBFOLDER = '';
+
+//DB Params
+const DB_HOST = 'localhost';
+const DB_USER = 'root';
+const DB_PASS = '';
+const DB_NAME = 'test';

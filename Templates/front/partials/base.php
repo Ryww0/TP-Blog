8dd7c64e075104e8b794a36de9298e052b970758
Nitha Thghm <t.nithachan@gmail.com>
2022-09-14 14:35:34 +0200

    file header.php modified

--- Templates/front/partials/base.php
+++ Templates/front/partials/base.php
@@ -1,6 +1,6 @@
 <?php
 
-//use App\Service\Session;
+use App\Service\Session;
 
 ?>
 
--- Templates/front/partials/header.php
+++ Templates/front/partials/header.php
@@ -1,1 +1,16 @@
 <?php
+
+use App\Service\Session;
+
+?>
+
+<header>
+    <nav class="navbar bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="<?= URL_ROOT ?>">Home</a>
+        </div>
+        <button type="button" class="btn btn-primary">Connexion</button>
+        <button type="button" class="btn btn-success">Inscription</button>
+    </nav>
+    <?php Session::showMessage(); ?>
+</header>
